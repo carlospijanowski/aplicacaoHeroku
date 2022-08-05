96c45bccea5ad945d05a08205231164aa4d7382e
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2022-08-04 22:24:03 -0300

    ajuste classe controller para testar no heroko

--- src/main/java/com/example/apague/ApagueApplication.java
+++ src/main/java/com/example/apague/ApagueApplication.java
@@ -2,25 +2,10 @@ package com.example.apague;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class ApagueApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApagueApplication.class, args);
-    }
-
-    @RequestMapping("/")
-    public String pagina1(){
-        return "pagina sem layout 2";
-    }
-
-    @RequestMapping("/teste/{parametro}")
-    public String pagina2(@PathVariable(value = "parametro")String parametro){
-        return "pagina teste" + parametro;
     }
 }
---
+++ src/main/java/com/example/apague/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.example.apague.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String pagina1(){
+        return "pagina sem layout 2";
+    }
+
+    @RequestMapping("/teste/{parametro}")
+    public String pagina2(@PathVariable(value = "parametro")String parametro){
+        return "pagina teste" + parametro;
+    }
+
+
+}
