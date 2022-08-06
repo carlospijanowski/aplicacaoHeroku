318c6b0e31b1b21fcaac459cdd724e3ab148e08b
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2022-08-05 22:18:40 -0300

    controller novo para teste de requisicao

--- src/main/java/com/example/apague/controller/MainController.java
+++ src/main/java/com/example/apague/controller/MainController.java
@@ -1,12 +1,19 @@
 package com.example.apague.controller;
 
+import com.example.apague.model.ProdutoDTO.java.Produto;
+import com.example.apague.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 public class MainController {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
 
     @Value("${nome}")
     String teste;
@@ -19,6 +26,16 @@ public class MainController {
     @RequestMapping("/teste/{parametro}")
     public String pagina2(@PathVariable(value = "parametro")String parametro){
         return "pagina teste" + parametro;
+    }
+
+    @PostMapping("/cadastrar")
+    public Produto cadastrarProduto(@RequestBody Produto produto){
+        return produtoRepository.cadastrarUmProduto(produto);
+    }
+
+    @GetMapping("/produtos")
+    public List<Produto> listaTodosOsProdutosCadastrados(){
+        return produtoRepository.listarTodosOsProdutos();
     }
 
 
