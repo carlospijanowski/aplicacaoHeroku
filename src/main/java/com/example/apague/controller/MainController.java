b3d6305aa22607e9af499702bc15e6a402c97c31
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2023-06-23 19:22:43 -0300

    teste limpando o projeto

--- src/main/java/com/example/apague/controller/MainController.java
+++ src/main/java/com/example/apague/controller/MainController.java
@@ -1,41 +1,17 @@
 package com.example.apague.controller;
 
-import com.example.apague.model.ProdutoDTO.java.Produto;
-import com.example.apague.repository.ProdutoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping()
 public class MainController {
 
-    @Autowired
-    ProdutoRepository produtoRepository;
 
-    @Value("${nome}")
-    String teste;
-
-    @RequestMapping("/")
-    public String pagina1(){
-        return "pagina sem layout no main "+teste;
-    }
-
-    @RequestMapping("/teste/{parametro}")
-    public String pagina2(@PathVariable(value = "parametro")String parametro){
-        return "pagina teste" + parametro;
-    }
-
-    @PostMapping("/cadastrar")
-    public Produto cadastrarProduto(@RequestBody Produto produto){
-        return produtoRepository.cadastrarUmProduto(produto);
-    }
-
-    @GetMapping("/produtos")
-    public List<Produto> listaTodosOsProdutosCadastrados(){
-        return produtoRepository.listarTodosOsProdutos();
+    @GetMapping("/teste")
+    public String teste() {
+        return "funfando o GEEEEEEEEEEEET !!!!!!!!!!";
     }
 
 
